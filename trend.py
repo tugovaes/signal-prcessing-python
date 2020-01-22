7f7cb648280174ba7c9001a0586d2a699d2f6537
tugovaes <52153244+tugovaes@users.noreply.github.com>
2020-01-22 16:48:09 +0500

    Update trend.py

--- trend.py
+++ trend.py
@@ -7,7 +7,9 @@ Created on Wed Jan 22 16:04:01 2020
 
 import pandas as pd
 import numpy as np
-df = pd.DataFrame({'B': [0, 1, 2,3, 4]})
+s=[1, 2, 3, 2, 101, 25]
+df = pd.DataFrame({dates : s})
+#df = pd.Serias(s)
 #s=({1, 2, 3, 2, 101, 25})
 trend = df.rolling(2).sum()
 
